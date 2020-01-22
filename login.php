f13cc126f85da0a5724d02a3f5ec2bb679fa406e
Nayan3005 <43439667+Nayan3005@users.noreply.github.com>
2020-01-22 13:00:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -11,34 +11,22 @@
 
 <div class="container login-container">
             <div class="row">
+			<div class="col-md-3"></div>
                 <div class="col-md-6 login-form-1">
-                    <h3>Lawyer Login</h3>
-                    <form action="lawyer-login.php" method="POST">
+                    <h3>Login</h3>
+                    <form action="LoginCode.php" method="POST">
                         <div class="form-group">
-                            <input type="text" class="form-control" name="email" placeholder="Your Email *" value="" />
+                            <input type="text" class="form-control" name="email" placeholder="Your Email *" value="" required />
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" name="pass" placeholder="Your Password *" value="" />
+                            <input type="password" class="form-control" name="pass" placeholder="Your Password *" value="" required/>
                         </div>
                         <div class="form-group">
                             <input type="submit" class="btnSubmit" value="Login" />
                         </div>
                     </form>
                 </div>
-                <div class="col-md-6 login-form-2">
-                    <h3>Client Login</h3>
-                    <form action="client-login.php" method="POST">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Your Email *" value="" name="email"/>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Your Password *" value="" name="pass"/>
-                        </div>
-                        <div class="form-group">
-							<input  onclick="client()"; type="submit" class="btnSubmit" value="Login" />
-                        </div>
-                    </form>
-                </div>
+                
             </div>
         </div>
 
